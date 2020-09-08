2620e9614347be1bfce7baadae83f99521eb0658
chamrouensann <chamrouensann.geek@gmail.com>
2020-09-08 11:17:01 +0700

    Changing from main to stateless widget

--- Exercise001/hello_world/lib/main.dart
+++ Exercise001/hello_world/lib/main.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "Flutter Application",
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      backgroundColor: Colors.green,
-      accentColor: Colors.deepPurpleAccent,
-    ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter"),
-        backgroundColor: Colors.red,
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Application",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        backgroundColor: Colors.green,
+        accentColor: Colors.deepPurpleAccent,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("I love App Development and Cambodia"),
-            Text("I love App Development."),
-            RaisedButton(
-              onPressed: () {},
-              child: Text("Login"),
-              color: Colors.redAccent,
-              splashColor: Colors.blue,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("I love App Development and Cambodia"),
+              Text("I love App Development."),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("SignUp"),
+                color: Colors.redAccent,
+                splashColor: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.add_a_photo,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-          Icons.add_a_photo,
-        ),
-      ),
-    ),
-  );
-  runApp(app);
+    );
+  }
 }
